f8c649b1692578bf2373a181cd5591044f724ecd
lvxinjia <3241132103@qq.com>
2024-01-09 21:09:21 +0800

    kaoqin-data

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <p>{{ apple }}</p>
+    <button @click="a">点击</button>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// @ is an alias to /src
+const apple = ref("苹果")
+function a(e){
+  apple.value = "香蕉"
+}
+</script>
